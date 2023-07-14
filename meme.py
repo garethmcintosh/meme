fbe36432d94bb81cbb77c395c33579334124d361
garethmcintosh <gazzadamazza@gmail.com>
2023-07-15 05:13:04 +1200

    added text wrapping & fixed a number of bugs

--- meme.py
+++ meme.py
@@ -5,37 +5,57 @@ from PIL import Image, ImageDraw, ImageFont
 import cv2
 import numpy as np
 import imageio
+import textwrap
+import warnings
 
+warnings.filterwarnings('ignore')
 
-# Function to calculate the text size
-def scale_font(draw, text, box_width, box_height, max_font_size=None):
-    font_size = 1
+# Function to calculate the text size and wrap it
+def fit_text_to_box(draw, text, box_width, box_height, max_font_size=None):
+    font_size = max_font_size or 100  # Start with a larger font size
+    min_font_size = 10  # Don't go below this font size
+    step_size = 10  # Adjust font size by this step size
     font = ImageFont.truetype("arial.ttf", font_size)
-    while True:
-        w, h = draw.textsize(text, font=font)
-        if (w < box_width and h < box_height) and (
-            not max_font_size or font_size < max_font_size
-        ):
-            font_size += 1
-            font = ImageFont.truetype("arial.ttf", font_size)
-        else:
-            return font
+
+    while font_size >= min_font_size:
+        text_lines = textwrap.wrap(text, width=int(box_width / (font_size * 0.45)))  # Reduced divisor for wider lines
+        line_heights = [draw.textsize(line, font=font)[1] for line in text_lines]
+        line_widths = [draw.textsize(line, font=font)[0] for line in text_lines]
+        max_line_width = max(line_widths)
+        total_height = sum(line_heights)
+
+        if max_line_width <= box_width and total_height <= box_height:
+            # If the text fits, we're done
+            return font, text_lines
+
+        # If the text doesn't fit, decrease the font size
+        font_size -= step_size
+        font = ImageFont.truetype("arial.ttf", font_size)
+
+    print("Text can't fit into the box, even at the smallest font size.")
+    return font, text_lines
+
+
 
 
 def process_frame(img, args):
     if args.type == "caption":
-        caption_height = int(img.height * 0.15)
+        caption_height = int(img.height * 0.075)
         caption_box = (img.width, caption_height)
         caption_img = Image.new("RGB", caption_box, color=(255, 255, 255))
         draw = ImageDraw.Draw(caption_img)
-        font = scale_font(draw, args.text, img.width - 20, caption_height)
-        text_height = font.getsize(args.text)[1]
-        if args.position == "top":
-            text_position = (10, (caption_height - text_height) // 2)
-        elif args.position == "bottom":
-            text_position = (10, caption_height - text_height - 25)
+        font, text_lines = fit_text_to_box(draw, args.text, img.width / 1.25, img.height // 2)
         black = (0, 0, 0)
-        draw.text(text_position, args.text, fill=black, font=font)
+
+        for i, line in enumerate(text_lines):
+            text_width, text_height = draw.textsize(line, font=font)
+            if args.position == "top":
+                text_position = (10, i * text_height)
+            elif args.position == "bottom":
+                text_position = (10, caption_height - text_height * (len(text_lines) - i))
+
+            draw.text(text_position, line, fill=black, font=font)
+
         final_img = Image.new("RGB", (img.width, img.height + caption_height))
         if args.position == "top":
             final_img.paste(caption_img, (0, 0))
@@ -43,48 +63,53 @@ def process_frame(img, args):
         elif args.position == "bottom":
             final_img.paste(img, (0, 0))
             final_img.paste(caption_img, (0, img.height))
+
     elif args.type == "overlay":
         final_img = img.copy()
         draw = ImageDraw.Draw(final_img)
-        font = scale_font(draw, args.text, img.width / 1.25, img.height // 2)
-        text_width, text_height = font.getsize(args.text)
-        if args.position == "top":
-            text_position = ((img.width - text_width) // 2, img.height // 15)
-        elif args.position == "bottom":
-            text_position = (
-                (img.width - text_width) // 2,
-                img.height - text_height - img.height // 8,
-            )
+        font, text_lines = fit_text_to_box(draw, args.text, img.width // 1.25, img.height // 2)
         white = (255, 255, 255)
         black = (0, 0, 0)
         stroke_width = 2
-        draw.text(
-            text_position,
-            args.text,
-            fill=white,
-            font=font,
-            stroke_width=stroke_width,
-            stroke_fill=black,
+
+        for i, line in enumerate(text_lines):
+            text_width, text_height = draw.textsize(line, font=font)
+            if args.position == "top":
+                text_position = ((img.width - text_width) // 2, img.height // 15 + i * text_height)
+            elif args.position == "bottom":
+                text_position = (
+                    (img.width - text_width) // 2,
+                    img.height - text_height * (len(text_lines) - i) - img.height // 8,
+                )
+            draw.text(
+                text_position,
+                line,
+                fill=white,
+                font=font,
+                stroke_width=stroke_width,
+                stroke_fill=black,
         )
+
     else:
         print(f"Invalid meme type: {args.type}")
         exit(1)
 
     draw = ImageDraw.Draw(final_img)
-    font = scale_font(draw, args.mark, img.width // 4, img.height // 4, max_font_size=15)
-    text_width, text_height = font.getsize(args.mark)
+    font, watermark_lines = fit_text_to_box(draw, args.mark, img.width // 2, img.height // 2, max_font_size=25)
 
     # Create a centered box and then calculate a random position for watermark inside it
     center_x, center_y = final_img.width // 2, final_img.height // 2
-    box_width, box_height = img.width // 4, img.height // 4
+    box_width, box_height = img.width // 2, img.height // 2  # Increased box size
     min_x = center_x - box_width // 2
     min_y = center_y - box_height // 2
 
-    random_x = random.randint(min_x, min_x + box_width - text_width)
-    random_y = random.randint(min_y, min_y + box_height - text_height)
-    watermark_position = (random_x, random_y)
-    white = (255, 255, 255)
-    draw.text(watermark_position, args.mark, fill=white, font=font)
+    for i, line in enumerate(watermark_lines):
+        text_width, text_height = font.getsize(line)
+        random_x = random.randint(min_x, min_x + box_width - text_width)
+        random_y = random.randint(min_y, min_y + box_height - text_height)
+        watermark_position = (random_x, random_y + i * text_height)  # Adjust y position based on line number
+        white = (255, 255, 255)
+        draw.text(watermark_position, line, fill=white, font=font)
 
     return final_img
 
